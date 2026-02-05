4eca037cdf2ca1d3d8844d6d5f780c7ac1bd3d60
Sdsquires27 <89485340+Sdsquires27@users.noreply.github.com>
2026-02-05 12:13:55 -0700

    Reorganized things; created app components

--- src/app.jsx
+++ src/app.jsx
@@ -3,14 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">
+  return (
+  <div className="body bg-dark text-light">
     <header>
         <h1>Registry</h1>
         <nav>
             <menu className="header-menu">
                 <li><a className="nav-link active" href="index.html">Home</a></li>
                 <li><a className="nav-link active" id="view-registry" href="my-registry.html">View</a></li>
-                <li><a className="nav-link active" id="view-other-registries" href="view-registry.html">Other</a></li>
+                <li><a className="nav-link active" id="view-other-regfistries" href="view-registry.html">Other</a></li>
                 <li><a className="nav-link active" href="about.html">About</a></li>
             </menu>
         </nav>
@@ -19,7 +20,7 @@ export default function App() {
     <footer>
         <p>Author Name: Seth Squires</p>
         <a className="nav-link active" id="github-link" href="https://github.com/Sdsquires27/startup">View GitHub</a>
-
     </footer>
-  </div>;
+  </div>
+  );
 }

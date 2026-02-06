b06178f72100cf29c18c83e36e456b58e7fdd909
Sdsquires27 <89485340+Sdsquires27@users.noreply.github.com>
2026-02-05 18:11:01 -0700

    my-registry.jsx created

---
+++ src/my-registry/my-registry.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function MyRegistry() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>registry displayed here</div>
+    </main>
+  );
+}

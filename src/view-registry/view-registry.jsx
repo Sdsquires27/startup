8e4e2813d5058f1f3dcade7e35e3642ed757216c
Sdsquires27 <89485340+Sdsquires27@users.noreply.github.com>
2026-02-05 18:11:56 -0700

    Made about.jsx and view-registry.jsx

---
+++ src/view-registry/view-registry.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function ViewRegistry() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>view registry displayed here</div>
+    </main>
+  );
+}

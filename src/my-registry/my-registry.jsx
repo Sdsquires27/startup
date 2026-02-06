f58516284da5a8797073e705d1cc6620a16d93d3
Sdsquires27 <89485340+Sdsquires27@users.noreply.github.com>
2026-02-05 18:24:18 -0700

    Added references to .css files

--- src/my-registry/my-registry.jsx
+++ src/my-registry/my-registry.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './my-registry.css';
 
 export function MyRegistry() {
   return (
-    <main className="container-fluid text-center">
+    <main className="container-fluid body text-center">
       <div>registry displayed here</div>
     </main>
   );
--- src/view-registry/view-registry.jsx
+++ src/view-registry/view-registry.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './view-registry.css';
 
 export function ViewRegistry() {
   return (

4d85a259b37faa06661dafae2994f398b8b1dff8
Sdsquires27 <89485340+Sdsquires27@users.noreply.github.com>
2026-02-07 12:57:33 -0700

    Light formatting changes

--- src/app.jsx
+++ src/app.jsx
@@ -10,7 +10,7 @@ import { About } from './about/about';
 export default function App() {
   return (
     <BrowserRouter>
-        <div className="body bg-dark text-light">
+        <div className="body">
             <header>
                 <h1>Registry</h1>
                 <nav>

ded0f64d483e36522ec36a909cf6d2a427151942
Sdsquires27 <89485340+Sdsquires27@users.noreply.github.com>
2026-02-07 13:31:11 -0700

    formatted about.jsx

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 export function About() {
   return (
-    <main className="container-fluid text-center">
-      <div>about displayed here</div>
+    <main>
+        <hr />
+        <div>
+            <p>For many, gift giving--and receiving--isn't as fun as others make it out to be. It can be hard to pick out the right gift for a person, and it is all too common that someone receieves a gift that they didn't want or need--or, yet worse, they receive something that they already had! The Registry application simplifies this process: people will be able to create lists of things that they want as gifts and others will be able to view and claim these gifts to buy. Think of it as somehwat similar to other registries, but constantly up and ready to be used. This ensures that one can purchase the gifts that their friends and family actually want.</p>
+        </div>
+        <hr />
     </main>
   );
 }

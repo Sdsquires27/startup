d2f83826e8f3c2568cf8b67dd5a3dcdba00ca957
Sdsquires27 <89485340+Sdsquires27@users.noreply.github.com>
2026-02-12 11:38:00 -0700

    Changed about.css in the simon files

--- simon-css/src/about/about.jsx
+++ simon-css/src/about/about.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import './about.css';
 
-export function About() {
-  return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>
-        <div id="picture" className="picture-box"><img src="placeholder.jpg" alt="random" /></div>
+export function About(props) {
+  const [imageUrl, setImageUrl] = React.useState('');
+  const [quote, setQuote] = React.useState('Loading...');
+  const [quoteAuthor, setQuoteAuthor] = React.useState('unknown');
 
+  React.useEffect(() => {
+    setImageUrl(`placeholder.jpg`);
+    setQuote('Show me the code');
+    setQuoteAuthor('Linus Torvalds');
+  }, []);
+
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>
+        <div id='picture' className='picture-box'>
+          <img src={imageUrl} alt='random image' />
+        </div>
         <p>
           Simon is a repetitive memory game where you follow the demonstrated color sequence until you make a mistake.
           The longer the sequence you repeat, the greater your score.
@@ -17,9 +28,9 @@ export function About() {
           educational use only. No part of this code or program should be used outside of that definition.
         </p>
 
-        <div id="quote" className="quote-box bg-light text-dark">
-          <p className="quote">Words are cheap. Show me the code.</p>
-          <p className="author">Linus Torvalds</p>
+        <div className='quote-box bg-light text-dark'>
+          <p className='quote'>{quote}</p>
+          <p className='author'>{quoteAuthor}</p>
         </div>
       </div>
     </main>

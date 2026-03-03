9b72c7a6ed225daf7deba090fc60629d476588aa
Sdsquires27 <89485340+Sdsquires27@users.noreply.github.com>
2026-03-03 14:45:24 -0700

    Created first steps toward backend changes and started listening on port 4000

--- service/index.js
+++ service/index.js
@@ -1,2 +1,13 @@
+const express = require('express');
+const app = express();
+
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 app.use(express.static('public'));
+
+app.get('*splat', (_req, res) => {
+  res.send({ msg: 'Simon service' });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});

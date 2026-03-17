205e11f35393ae5c88a1c3dc09016a40bf97bdd9
Sdsquires27 <89485340+Sdsquires27@users.noreply.github.com>
2026-03-16 20:17:12 -0600

    Added start of database; set up data structures that will be needed

---
+++ service/database.js
@@ -0,0 +1,24 @@
+const {MongoClient} = require("mongodb");
+const config = require("./dbConfig.json");
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('startup');
+const userCollection = db.collection("user");
+const userItems = db.collection("userItems");
+const claimStatuses = db.collections("claimStatuses");
+
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+})();
+
+function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
+
+

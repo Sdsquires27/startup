b7263d83bf79c12aab446c6c9962d9447b0f5855
Sdsquires27 <89485340+Sdsquires27@users.noreply.github.com>
2026-03-24 11:54:12 -0600

    Added RegistryHandler

--- src/view-registries/RegistryHandlers.jsx
+++ src/view-registries/RegistryHandlers.jsx
@@ -9,3 +9,46 @@ export async function removeRegistryItem(itemIndex, username, updateFunction) {
   });
   updateFunction();
 }
+
+class RegistryHandler
+{
+  handlers = [];
+
+  constructor() 
+  {
+    let port = window.location.port;
+    const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+    this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}`);
+
+    this.socket.onopen = () => {
+      console.log('Connected to item sync');
+    };
+    this.socket.onclose = () => {
+      console.log('Disconnected from item sync');
+    };
+    this.socket.onmessage = async (msg) => {
+      try {
+        const event = JSON.parse(await msg.data.text());
+        this.receiveEvent(event);
+      } catch {}
+    };
+  }
+    broadcastEvent(type, payload) {
+    const event = { type, payload };
+    this.socket.send(JSON.stringify(event));
+  }
+
+  addHandler(handler) {
+    this.handlers.push(handler);
+  }
+
+  removeHandler(handler) {
+    this.handlers = this.handlers.filter((h) => h !== handler);
+  }
+
+  receiveEvent(event) {
+    this.handlers.forEach((handler) => handler(event));
+  }
+}
+
+export const registryHandler = new RegistryHandler();
